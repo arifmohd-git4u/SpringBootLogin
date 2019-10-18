986e44f1ae9c4eff6caec174bcbddc9ed52eb391
Mohd Arif <explore.mohd.arif8810@gmail.com>
2019-10-18 11:57:51 +0530

    Initial Commit

---
+++ src/main/java/com/springboot/demo/controllers/SimpleController.java
@@ -0,0 +1,25 @@
+package com.springboot.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springboot.demo.models.Student;
+
+@Controller
+public class SimpleController {
+	@RequestMapping("/")
+	public String homePage() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute Student student) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("user-data");
+		modelAndView.addObject("student", student);
+		return modelAndView;
+	}
+}
